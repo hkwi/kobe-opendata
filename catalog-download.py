3c3178a748ef52125e49e157f55b2c6ddd66967e
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2015-09-15 14:20:59 +0900

    remove geojson

--- catalog-download.py
+++ catalog-download.py
@@ -5,6 +5,6 @@ import urllib.request
 
 for p in csv.DictReader(open("catalog-download.csv")):
 	url = p["URL"]
-	with open(os.path.join("mirror", os.path.basename(url)), "wb") as out:
+	with open(os.path.join("import/catalog", os.path.basename(url)), "wb") as out:
 		for data in urllib.request.urlopen(url):
 			out.write(data)
