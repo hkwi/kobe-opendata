2dc2c92d1f95e9fe7f4a6b85538938aa1c0538cf
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-01-07 23:20:56 +0900

    catalog.csv is utf8

--- catalog-download.py
+++ catalog-download.py
@@ -3,7 +3,7 @@ import sys
 import os.path
 import urllib.request
 
-for p in csv.DictReader(open("catalog-download.csv")):
+for p in csv.DictReader(open("catalog-download.csv", encoding="UTF-8")):
 	url = p["URL"]
 	with open(os.path.join("import/catalog", os.path.basename(url)), "wb") as out:
 		for data in urllib.request.urlopen(url):
