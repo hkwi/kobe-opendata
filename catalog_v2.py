dda36fa41d7d1afafda2c9a86b3999cdaacea8e2
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-04-19 00:53:12 +0900

    sync

--- catalog_v2.py
+++ catalog_v2.py
@@ -14,9 +14,18 @@ except:
 atom_url = "https://data.city.kobe.lg.jp/data/feeds/dataset.atom"
 NS = dict(f="http://www.w3.org/2005/Atom")
 dataset = []
-for e in ET.parse(urlopen(atom_url)).findall(".//f:entry", NS):
-	ds = json.load(urlopen(e.find('f:link[@rel="enclosure"]', NS).get("href")))
-	dataset.append(ds)
+while atom_url:
+	print atom_url
+	doc = ET.parse(urlopen(atom_url))
+	for e in doc.findall(".//f:entry", NS):
+		ds = json.load(urlopen(e.find('f:link[@rel="enclosure"]', NS).get("href")))
+		dataset.append(ds)
+	
+	nx = doc.find('.//f:link[@rel="next"]', NS)
+	if nx is None:
+		break
+	else:
+		atom_url = nx.get("href")
 
 s = yaml.safe_dump_all(dataset, allow_unicode=True, default_flow_style=False)
 with open("catalog_v2.yml", "wb") as f:
@@ -28,5 +37,5 @@ with open("catalog_v2_resources.txt","w") as fp:
 		for r in ds["resources"]:
 			if r["format"].lower() == "html":
 				continue
-			assert r["format"].lower() in "csv xls xlsx ppt pptx pdf".split(), r["format"]
+			assert r["format"].lower() in "csv xls xlsx ppt pptx pdf xml".split(), r["format"]
 			w.writerow((r["url"],))
