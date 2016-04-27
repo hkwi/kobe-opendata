febda5284bf4914f108b8e669595f9a697fef807
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2016-04-27 14:04:48 +0900

    migrate to python3

--- catalog.py
+++ catalog.py
@@ -3,36 +3,66 @@
 # http://www.city.kobe.lg.jp/information/opendata/catalogue.html
 # をスクレイピングして、一覧登録されているものを csv として抜き出します。
 # 
+from __future__ import print_function
+import yaml
 import sys
 import csv
 import lxml.html
-try:
-	"".decode("UTF-8")
-	u2b = lambda x:x.encode("UTF-8")
-	b2u = lambda x:x.decode("UTF-8")
-except:
-	u2b = b2u = lambda x:x
-
-lmap = lambda f,x:list(map(f,x))
 # lxml likes unicode type, csv likes bytes type
 
-dout = csv.writer(open("catalog-download.csv", "w"))
-pout = csv.writer(open("catalog-publish.csv", "w"))
+dout = csv.writer(open("catalog-download.csv", "w", newline=""))
+pout = csv.writer(open("catalog-publish.csv", "w", newline=""))
 
 fields = None
 r = lxml.html.parse('http://www.city.kobe.lg.jp/information/opendata/catalogue.html').getroot()
 for e in r.xpath("//h2[@id='midashi356']/following-sibling::table//tr"):
 	th = ["".join(x.xpath(".//text()")).strip() for x in e.xpath("td")]
+	names = "データタイトル ライセンス タグ 形式 データ時点 担当局室 所管課".split()
 	if fields is None:
-		names = ["データタイトル", "ライセンス", "タグ", "形式", "データ時点", "担当局室", "所管課"]
-		names = lmap(b2u, names)
 		fields = [th.index(key) for key in names]
 		
-		dout.writerow(lmap(u2b, names+[b2u("URL")]))
-		pout.writerow(lmap(u2b, names+[b2u("URL")]))
+		dout.writerow(names+["URL"])
+		pout.writerow(names+["URL"])
 	else:
 		for url in e.xpath("td[8]//a/@href"):
-			dout.writerow(lmap(u2b, [th[i] for i in fields]+[url]))
+			dout.writerow([th[i] for i in fields]+[url])
+			print(url)
 		for url in e.xpath("td[9]//a/@href"):
-			pout.writerow(lmap(u2b, [th[i] for i in fields]+[url]))
+			pout.writerow([th[i] for i in fields]+[url])
+
+names = None
+data = []
+for e in r.xpath("//h2[@id='midashi356']/following-sibling::table//tr"):
+	if names is None:
+		names = ["".join(x.xpath(".//text()")).strip() for x in e.xpath("td")]
+		continue
+	
+	obj = dict()
+	es = [x for x in e.xpath("td")]
+	for i,name in enumerate(names):
+		if name == "ダウンロード":
+			e = es[i].find(".//a")
+			if e is not None:
+				obj["ダウンロード"] = e.get("href")
+		elif name == "掲載ページ":
+			e = es[i].find(".//a")
+			if e is not None:
+				obj["掲載ページ"] = e.get("href")
+		elif name == "ライセンス":
+			if es[i].xpath('.//img[@src="http://www.city.kobe.lg.jp/information/opendata/img/by.png"]'):
+				obj["ライセンス"] = "CC-BY"
+			elif es[i].xpath('.//img[@src="http://www.city.kobe.lg.jp/information/opendata/img/cc-by-sa.png"]'):
+				obj["ライセンス"] = "CC-BY-SA"
+			elif es[i].xpath('.//img[@alt="by-nc"]'):
+				obj["ライセンス"] = "CC-BY-NC"
+			elif "".join(es[i].xpath(".//text()")).strip() == "著作物ではありません":
+				obj["ライセンス"] = "CC0"
+		else:
+			obj[name] = "".join(es[i].xpath(".//text()")).strip()
+	
+	data.append(obj)
+
+s = yaml.safe_dump_all(data, allow_unicode=True, default_flow_style=False)
+with open("catalog.yml", "w") as fp:
+	fp.write(s)
 
--- catalog_v2.py
+++ catalog_v2.py
@@ -2,6 +2,7 @@
 #
 # For data.city.kobe.lg.jp release
 #
+from __future__ import print_function
 import yaml
 import json
 import xml.etree.ElementTree as ET
@@ -15,10 +16,11 @@ atom_url = "https://data.city.kobe.lg.jp/data/feeds/dataset.atom"
 NS = dict(f="http://www.w3.org/2005/Atom")
 dataset = []
 while atom_url:
-	print atom_url
+	print(atom_url)
 	doc = ET.parse(urlopen(atom_url))
 	for e in doc.findall(".//f:entry", NS):
-		ds = json.load(urlopen(e.find('f:link[@rel="enclosure"]', NS).get("href")))
+		data = urlopen(e.find('f:link[@rel="enclosure"]', NS).get("href")).read()
+		ds = json.loads(data.decode("UTF-8"))
 		dataset.append(ds)
 	
 	nx = doc.find('.//f:link[@rel="next"]', NS)
@@ -28,7 +30,7 @@ while atom_url:
 		atom_url = nx.get("href")
 
 s = yaml.safe_dump_all(dataset, allow_unicode=True, default_flow_style=False)
-with open("catalog_v2.yml", "wb") as f:
+with open("catalog_v2.yml", "w", encoding="UTF-8", newline="") as f:
 	f.write(s)
 
 with open("catalog_v2_resources.txt","w") as fp:
